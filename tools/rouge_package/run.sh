0671966eb3991364a3f6468ab0f9032c466de4ce
stuborn-akshii <87079015+stuborn-akshii@users.noreply.github.com>
2022-10-15 00:56:54 +0530

    Changes regard to Google Colab
    Update run.sh in such way it should run in google colab directly

--- tools/rouge_package/run.sh
+++ tools/rouge_package/run.sh
@@ -1,14 +1,13 @@
-cp ROUGE-1.5.5.tar.gz ~
-tar -xvf ~/ROUGE-1.5.5.tar.gz --directory ~
-rm ~/ROUGE-1.5.5.tar.gz
-sudo apt install libxml-parser-perl
-sudo pip3 install pyrouge
+!cp ROUGE-1.5.5.tar.gz ~
+!tar -xvf  'ROUGE-1.5.5.tar.gz' -C '/content/drive/MyDrive/NATS/tools/rouge_package/'
+!rm ROUGE-1.5.5.tar.gz
+!sudo apt install libxml-parser-perl
+!sudo pip3 install pyrouge
+!cd ~
+!mkdir .pyrouge
+!echo [pyrouge settings] >> .pyrouge/settings.ini
+!echo home_dir = $PWD/ROUGE-1.5.5 >> .pyrouge/settings.ini
+%cd /content/drive/MyDrive/NATS/tools/rouge_package/ROUGE-1.5.5/data
 
-cd ~
-mkdir .pyrouge
-echo [pyrouge settings] >> .pyrouge/settings.ini
-echo home_dir = $PWD/ROUGE-1.5.5 >> .pyrouge/settings.ini
-
-cd ~/ROUGE-1.5.5/data/
-rm WordNet-2.0.exc.db
-./WordNet-2.0-Exceptions/buildExeptionDB.pl ./WordNet-2.0-Exceptions ./smart_common_words.txt ./WordNet-2.0.exc.db
+!rm WordNet-2.0.exc.db
+!./WordNet-2.0-Exceptions/buildExeptionDB.pl ./WordNet-2.0-Exceptions ./smart_common_words.txt ./WordNet-2.0.exc.db
